70d52e75ccecdb808dea8f3061307577a099b03f
Alfian Losari <alfianlosari@gmail.com>
2020-03-26 14:31:08 +0800

    Add keyboard show and hide notification content offset in StatsView

--- CoronaVirusTracker/Views/Search Bar/SearchBar.swift
+++ CoronaVirusTracker/Views/Search Bar/SearchBar.swift
@@ -11,42 +11,65 @@ import SwiftUI
 struct SearchBar : UIViewRepresentable {
     
     @Binding var text : String
+    @Binding var keyboardHeight: CGFloat
     var placeholder: String
     
     class Cordinator : NSObject, UISearchBarDelegate {
         
         @Binding var text : String
+        @Binding var searchBarHeight: CGFloat
+
         
-        init(text : Binding<String>) {
+        init(text : Binding<String>, searchBarHeight: Binding<CGFloat>) {
             _text = text
+            _searchBarHeight = searchBarHeight
+            
+            super.init()
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(keyboardWillShow),
+                name: UIResponder.keyboardWillShowNotification,
+                object: nil
+            )
+            
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(keyboardWillHide),
+                name: UIResponder.keyboardWillHideNotification,
+                object: nil
+            )
         }
         
         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             text = searchText
-
-            if searchText.isEmpty {
-                 searchBar.resignFirstResponder()
-             }
         }
+        
         
         func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
             searchBar.resignFirstResponder()
         }
         
-        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-            searchBar.resignFirstResponder()
+        @objc func keyboardWillShow(_ notification: Notification) {
+            if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+                let keyboardRectangle = keyboardFrame.cgRectValue
+                searchBarHeight = keyboardRectangle.height
+            }
         }
         
+        @objc func keyboardWillHide(_ notification: Notification) {
+            searchBarHeight = 0
+        }
     }
     
     func makeCoordinator() -> SearchBar.Cordinator {
-        return Cordinator(text: $text)
+        return Cordinator(text: $text, searchBarHeight: $keyboardHeight)
     }
     
     func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
         let searchBar = UISearchBar(frame: .zero)
         searchBar.placeholder = placeholder
         searchBar.delegate = context.coordinator
+        searchBar.enablesReturnKeyAutomatically = false
         return searchBar
     }
     
--- CoronaVirusTracker/Views/Stats/StatsView.swift
+++ CoronaVirusTracker/Views/Stats/StatsView.swift
@@ -12,6 +12,7 @@ import UIKit
 struct StatsView: View {
     
     @State private var searchTerm : String = ""
+    @State private var searchBarHeight: CGFloat = 0
 
     var body: some View {
         NavigationView {
@@ -19,12 +20,12 @@ struct StatsView: View {
                 TotalCountContainerView()
                     .padding(.top)
                     .padding(.horizontal)
-                SearchBar(text: $searchTerm, placeholder: "Search for a country")
+                SearchBar(text: $searchTerm, keyboardHeight: $searchBarHeight, placeholder: "Search for a country")
                     .padding(.horizontal).padding(.top)
                 CoronaCaseTableView(searchTerm: $searchTerm)
                     .padding(.horizontal)
+                    .padding(.bottom, searchBarHeight)
             }
-                 
             .background(Color(UIColor.secondarySystemBackground))
             .navigationBarTitle("Corona Virus Tracker", displayMode: .inline)
     
@@ -32,3 +33,4 @@ struct StatsView: View {
     }
     
 }
+
